61067c618173d0963252fef90a30224c161a49e5
Bee-jay <bjpaiz@gmail.com>
2021-09-03 23:34:09 +0100

    added functionality

--- calculator.js
+++ calculator.js
@@ -1,73 +1,58 @@
-const add = function(a, b) {
-    return a + b;
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const percent = (a) => a / 100;
+
+const inputNum = (e) => {
+    operands.unshift(parseInt(`${e.target.id}`));
+    equationArray.unshift(parseInt(`${e.target.id}`));
+    currentValue.textContent = parseInt(`${e.target.id}`);
+    lastInput = 'num'
 }
 
-const subtract = function(a, b) {
-    return a - b;
+const inputOp = (e) => {
+    if (lastInput == 'op') return;
+    operators.unshift(e.target.id);
+    equationArray.unshift(e.target.id);
+    lastInput = 'op'
 }
 
-const multiply = function(a, b) {
-    return a * b;
-}
-
-const divide = function(a, b) {
-    return a / b;
-}
-
-const percent = function(a) {
-    return a / 100;
-}
-
-function input(e) {
-    if (!isNaN(e.target.id)) {
-        operands.unshift(parseInt(`${e.target.id}`));
-        currentValue.textContent = parseInt(`${e.target.id}`);
-    } else if (e.target.id == 'equals') {
+const inputSpcl = (e) => {
+    if (e.target.id == 'equals') {
         operate();
-    } else {
-        operators.unshift(`${e.target.id}`);
+    } else if (e.target.id == 'all-clr') {
+        operands.splice(0, operands.length);
+        operators.splice(0, operators.length);
+        equationArray.splice(0, equationArray.length);
+        currentValue.textContent = '';
+        equationArray.textContent = '';
     }
-
+    lastInput = 'spcl'
 }
 
 function operate() {
     let result;
     if (operators[0] == '+') result = add(operands[1], operands[0]);
-    if (operators[0] == '-') result = subtract(operands[0], operands[1]);
-    if (operators[0] == '*') result = multiply(operands[0], operands[1]);
-    if (operators[0] == '/') result = divide(operands[0], operands[1]);
+    if (operators[0] == '-') result = subtract(operands[1], operands[0]);
+    if (operators[0] == '*') result = multiply(operands[1], operands[0]);
+    if (operators[0] == '/') result = divide(operands[1], operands[0]);
+    operands.splice(0, 1, result);
     currentValue.textContent = result;
 }
 
 const operands = [];
 const operators = [];
+const equationArray = [];
 const equation = document.querySelector('#equation');
-equation.textContent = '';
+equation.innerText = equationArray;
 const currentValue = document.querySelector('#currentValue');
 currentValue.textContent = '';
+let lastInput;
 
-const buttons = document.querySelectorAll('button');
-buttons.forEach(button => button.addEventListener('click', input))
-
-/*
-const allClr = document.querySelector('#all-clr');
-const clrEntry = document.querySelector('#clr-entry');
-const addition = document.querySelector('#add');
-const subtraction = document.querySelector('#subtract');
-const multiplication = document.querySelector('#multiply');
-const division = document.querySelector('#divide');
-const percentage = document.querySelector('#percent');
-const decimal = document.querySelector('#decimal');
-const one = document.querySelector('#one');
-const two = document.querySelector('#two');
-const three = document.querySelector('#three');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
-const seven = document.querySelector('#seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
-const zero = document.querySelector('#zero');
-const equals = document.querySelector('#equals');
-*/
-
+const numButtons = document.querySelectorAll('.num');
+numButtons.forEach(numButton => numButton.addEventListener('click', inputNum));
+const opButtons = document.querySelectorAll('.op');
+opButtons.forEach(opButton => opButton.addEventListener('click', inputOp));
+const spclButtons = document.querySelectorAll('.spcl');
+spclButtons.forEach(spclButton => spclButton.addEventListener('click', inputSpcl))
